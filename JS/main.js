fd7cfdf46c0d7b60751a800269bb53746d834a3a
Afzal97592 <95896597+Afzal97592@users.noreply.github.com>
2022-03-05 12:23:41 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+//Select tab content item
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    // add border to current tan
+    this.classList.add('tab-border');
+    //grab content item from dom
+    const tabContentItems = document.querySelector(`#${this.id}-content`);
+    // add show class
+    tabContentItems.classList.add('show');
+}
+function removeBorder(){
+    tabItems.forEach(item =>item.classList.remove('tab-border'));
+}
+function removeShow(){
+    tabContentItems.forEach(item =>item.classList.remove('show'));
+}
+//Listen for tab click
+tabItems.forEach(item => item.addEventListener('click',selectItem));
